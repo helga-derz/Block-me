a64cbb2a741bd4a197ae98ba34db3474bef342fb
Helga <ovbukanova@mail.ru>
2016-02-11 17:35:05 +0300

    reformat

--- game.py
+++ game.py
@@ -1,11 +1,16 @@
 import random
+import pygame
 
 size = 20
 
 class Object:
 
     def __init__(self):
-        pic = None
+        pass
+
+    def open_image(self, name, size=20):
+        image = pygame.image.load("image/" + name)
+        return pygame.transform.scale(image, (20, 20))
 
 
 class MovingObj(Object):
@@ -15,21 +20,24 @@ class MovingObj(Object):
 
     def move(self, string):
         if string == 'w':
-            self.y += 1
+            self.y += 20
         if string == 'a':
-            self.x -= 1
+            self.x -= 20
         if string == 'd':
-            self.x += 1
+            self.x += 20
         if string == 's':
-            self.y -= 1
+            self.y -= 20
 
 
 class Character(MovingObj):
 
     def __init__(self):
-        x = 9
-        y = 9
-        points = 0
+        self.x = 490
+        self.y = 290
+        self.points = 0
+        self.pic = self.open_image('cat.jpg')
+
+#    def update_character(self):
 
 
 class StaticObj(Object):
@@ -41,21 +49,12 @@ class StaticObj(Object):
 class Wall(StaticObj):
 
     def __init__(self):
-        pass
-
-
-class Bonus(StaticObj):
-
-    def __init__(self):
-        points = random.randint(1, 5)
-
-
-class Game:
+        self.pic = self.open_image('wall.jpeg')
 
     def create_edges(self):
 
         walls = []
-        
+
         for x in range(0, 1000, size):
             walls.append([x, 0])
             walls.append([x, 580])
@@ -75,3 +74,24 @@ class Game:
             walls.append(new_wall)
 
         return walls
+
+
+class Bonus(StaticObj):
+
+    def __init__(self):
+        self.points = random.randint(1, 5)
+        self.pic = self.open_image('bonus.png')
+
+    def update_bonuses(self, bonuses=[], new_bonus=[]):
+
+        if new_bonus:
+            bonuses.append(new_bonus)
+
+        return bonuses
+
+    def del_bonus(self, bonuses, eaten_bonus):
+        return bonuses.remove(eaten_bonus)
+
+
+class Game:
+    pass
--- main.py
+++ main.py
@@ -14,22 +14,11 @@ pygame.display.set_caption('13FPL LIFE')
 bg = Surface(size)
 bg.fill(Color(background_colour))
 
-# images
-
-wall_image = pygame.image.load("image/wall.jpeg")
-wall_image = pygame.transform.scale(wall_image, (20, 20))
-
-cat_image = pygame.image.load("image/cat.jpg")
-cat_image = pygame.transform.scale(cat_image, (20, 20))
-
-bonus_image = pygame.image.load("image/bonus.png")
-bonus_image = pygame.transform.scale(bonus_image, (20, 20))
-
 
 done = False
 Blocked = False
 
-gm = game.Game()
+wall = game.Wall()
 
 while not done and not Blocked:
     for event in pygame.event.get():
@@ -38,7 +27,7 @@ while not done and not Blocked:
 
     screen.blit(bg, (0, 0))  # filling screen with colour
 
-    for wall in gm.update_walls():
-        screen.blit(wall_image, (wall[0], wall[1]))
+    for x, y in wall.update_walls():
+        screen.blit(wall.pic, (x, y))
 
     pygame.display.update()
